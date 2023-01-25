bc1fc1335f881bb67ba5af09f12d82c794ea31c2
David Zhang <david.zny1022@gmail.com>
2023-01-25 12:19:18 -0800

    commit some default code for merge conflict testing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,18 @@
 const App = () => {
 
+  const testingArray = {
+    a: 'edit this',
+    b: 123
+    c: 'edit this',
+    d: 123
+  }
+
+
+  const mergeConflict = () => {
+    // edit line 13
+    return testingArray.a + testingArray.b
+  }
+
 
   return (
     <div>Testing 2</div>
