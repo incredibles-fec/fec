c2c03706947c6f708e14fb5ea62854954d9cde3c
kyrianite <jessicavu.od@gmail.com>
2023-01-25 15:48:29 -0800

    renamed eslint.js to .eslintrc.js and updated App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,10 @@
 const App = () => {
-
-  const testingArray = {
-    a: 'edit this',
-    b: 123
-    c: 'edit this',
-    d: 123
-  }
-
-
-  const mergeConflict = () => {
-    // edit line 13
-    return testingArray.a + testingArray.b
-  }
-
+  let testComponent = <productDetail />
 
   return (
-    <div>Testing Write Access</div>
+    <div>
+      {testComponent}
+    </div>
   )
 }
 
