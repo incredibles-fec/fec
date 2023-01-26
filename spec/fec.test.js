bb5c912be7f10aac5a2954e8aed0d1fe6472baee
Patrick Kelly <patrick.will.kelly@gmail.com>
2023-01-26 15:08:57 -0800

    install Jest

---
+++ spec/fec.test.js
@@ -0,0 +1,17 @@
+/* ---- FEC Testing Requirements --------------
+
+Controller functions (via unit tests)
+Custom models (via unit tests)
+React components (via unit tests)
+Seeding function (via unit tests)
+One end-to-end test for each service
+
+  ---------------------------------------------
+*/
+
+const sum = require('./testingFile');
+
+// first test attempt
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
