6878349d7dc03dd380b5ec8d69f2e65a2da70428
Patrick Kelly <patrick.will.kelly@gmail.com>
2023-01-29 09:38:57 -0800

    import css file into app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import ProductDetail from './components/ProductDetail/index.jsx';
 import RelatedOutfits from './components/itemsComparison/index.jsx';
 import QA from './components/QA/index.jsx';
 import Ratings from './components/ratings/index.jsx';
+import './assets/ro.css';
 
 const App = () => (
   <div>
