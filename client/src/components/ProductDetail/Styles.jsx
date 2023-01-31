3fce3f9a46ead47f84bb5758865bc58a6a6eb909
kyrianite <jessicavu.od@gmail.com>
2023-01-31 10:15:07 -0800

    fixed rounded thumbnail style images to be centered and cropped properly

--- client/src/components/ProductDetail/Styles.jsx
+++ client/src/components/ProductDetail/Styles.jsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 export default function ProductInformation({ styles }) {
   if (styles) {
     const allStyles = styles.map((p) => (
-      <div className="mask"><img className="style-item" src={p.photos[0].thumbnail_url} alt="" key={p.style_id} /></div>
+      <img className="style-item" src={p.photos[0].thumbnail_url} alt="" key={p.style_id} />
     ));
     return (
       <div>
