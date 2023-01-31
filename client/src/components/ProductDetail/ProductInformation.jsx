5443ef4cc1b06f19ff41ad532295d5e10f02bdb4
kyrianite <jessicavu.od@gmail.com>
2023-01-30 22:47:57 -0800

    show available styles per product

--- client/src/components/ProductDetail/ProductInformation.jsx
+++ client/src/components/ProductDetail/ProductInformation.jsx
@@ -1,15 +1,16 @@
 import React, { useState } from 'react';
 import AddToBag from './AddToBag.jsx';
 import ShareSocialMedia from './ShareSocialMedia.jsx';
+import Styles from './Styles.jsx';
 
-export default function ProductInformation({ product }) {
+export default function ProductInformation({ product, styles }) {
   return (
     <div>
       <div className="star-rating">Star Rating: &#9733; &#9734; &#9733; &#9734; &#9733;</div>
       <div className="product-category">Category: {product.category}</div>
       <div className="product-name"><h3>{product.name}</h3></div>
       <div className="product-price">${product.default_price}</div>
-      <div className="product=style">Style &gt; Selected Style </div>
+      <div className="product=style">Style &gt; Selected Style <Styles styles={styles} /></div>
       <div className="add-to-bag"><AddToBag /></div>
       <div className="share-to-social-media"><ShareSocialMedia /></div>
     </div>
--- client/src/components/ProductDetail/ShareSocialMedia.jsx
+++ client/src/components/ProductDetail/ShareSocialMedia.jsx
@@ -4,8 +4,8 @@ export default function ShareSocialMedia({ product }) {
   return (
     <div>
       <a href="#" className="fa fa-facebook"></a>
-      <a href="#" class="fa fa-twitter"></a>
-      <a href="#" class="fa fa-pinterest"></a>
+      <a href="#" className="fa fa-twitter"></a>
+      <a href="#" className="fa fa-pinterest"></a>
     </div>
   );
 }
