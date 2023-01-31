105fb8f11955d37611512e6cc05aad8adbf18888
Patrick Kelly <patrick.will.kelly@gmail.com>
2023-01-30 19:41:43 -0800

    implement layout of modal

--- client/src/components/itemsComparison/ProductModal.jsx
+++ client/src/components/itemsComparison/ProductModal.jsx
@@ -8,9 +8,32 @@ export default function ProductModal({ visible, onClick }) {
   return (
     <div>
       <div className="productModal">
-        <h1>Comparing</h1>
-        <h2>Modal Subheader</h2>
-        <button onClick={onClick} type="button">x</button>
+        <h1>COMPARING</h1>
+        <table>
+          <thead>
+            <th className="leftHeader">Current Product</th>
+            <th className="rightHeader">Compared Product</th>
+          </thead>
+        </table>
+        <table className="compareTable">
+          <thead>
+            <th className="checkLeftHeader" />
+            <th className="productInfo" />
+            <th className="checkRightHeader" />
+          </thead>
+          <tr>
+            <td className="checkLeft">checkmark</td>
+            <td className="productInfo">Sample product information</td>
+            <td className="checkRight">checkmark</td>
+          </tr>
+          <tr>
+            <td className="checkLeft">checkmark</td>
+            <td className="productInfo">Another detail</td>
+            <td className="checkRight">checkmark</td>
+          </tr>
+        </table>
+        {/* TODO: Update position to sit top right */}
+        <button className="modalExit" onClick={onClick} type="button">x</button>
       </div>
     </div>
   );
