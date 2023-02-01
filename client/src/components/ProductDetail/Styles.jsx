c3fbe830dbf2a1b9b58341b13f05465727172160
kyrianite <jessicavu.od@gmail.com>
2023-02-01 14:02:29 -0800

    improved position of currently selected style checkmark and set default loaded style

--- client/src/components/ProductDetail/Styles.jsx
+++ client/src/components/ProductDetail/Styles.jsx
@@ -2,28 +2,21 @@ import React from 'react';
 
 export default function Styles({ styles, currentStyle, setCurrentStyle }) {
   if (styles) {
-    // const allStyles = styles.map((p, index) => (
-    //   <div className="style-item-overlay" key={p.style_id} onClick={() => setCurrentStyle(styles[index])}>
-    //     <img className="style-item" src={p.photos[0].thumbnail_url} alt={p.name} />
-    //   </div>
-    // ));
-
     const rows = [...Array(Math.ceil(styles.length / 4))];
     const styleRows = rows.map((row, index) => styles.slice(index * 4, index * 4 + 4));
     const allStyles = styleRows.map((row, index) => (
-        <div className="styles-row" key={index}>{row.map((style) => (
-          <span className="style-item-overlay" key={style.style_id} onClick={() => setCurrentStyle(style)}>
-            <img className="style-item" src={style.photos[0].thumbnail_url} alt={style.name} />
-          </span>
-        ))}
+      <div className="styles-row" key={index}>{row.map((style) => (
+        <div className="style-item-overlay" key={style.style_id} onClick={() => setCurrentStyle(style)}>
+          <img className="style-item" src={style.photos[0].thumbnail_url} alt={style.name} />
+          {currentStyle.style_id === style.style_id ? <em className="checkmark">✓</em> : null}
         </div>
+      ))}
+      </div>
     ));
-    console.log(rows);
-    console.log(styleRows);
 
     return (
       <div>
-        <div className="product-style">Style &gt; {currentStyle.name} </div>
+        <div className="product-style">Style &gt; <u>{currentStyle.name}</u></div>
         {allStyles}
       </div>
     );
