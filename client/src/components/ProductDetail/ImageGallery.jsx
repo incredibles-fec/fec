db502b3cf20c30587bade8002b675007c84c3be2
kyrianite <jessicavu.od@gmail.com>
2023-02-03 01:35:12 -0800

    added zoom to display image, fixed bug on highlighting current thumbnail

--- client/src/components/ProductDetail/ImageGallery.jsx
+++ client/src/components/ProductDetail/ImageGallery.jsx
@@ -5,6 +5,7 @@ export default function ImageGallery({ style }) {
   const { name, photos } = style;
   const [index, setIndex] = useState(0);
   const [currentThumbnail, setCurrentThumbnail] = useState(photos[0].url);
+  const [normalView, setNormalView] = useState(true);
   const [expandedView, setExpandedView] = useState(false);
   const [zoomedView, setZoomedView] = useState(false);
 
@@ -14,6 +15,10 @@ export default function ImageGallery({ style }) {
       <img className="carousel-item-thumbnail" src={p.thumbnail_url} alt={name} />
     </div>
   ));
+
+  useEffect(() => {
+    setCurrentThumbnail(photos[0].url);
+  }, [style]);
 
   useEffect(() => {
     const prevArrows = document.querySelectorAll('.prev');
@@ -43,29 +48,45 @@ export default function ImageGallery({ style }) {
 
   const enlargeImage = () => {
     const img = document.getElementById('displayed-image');
-    if (expandedView) {
-      img.style.transform = 'scale(1)';
-      img.style.height = '500px';
-      img.style.width = '500px';
-      img.style.transition = 'transform 0.25s ease';
-      setExpandedView(false);
-    } else {
+    const carouselContainer = document.getElementById('carousel-container');
+    const imageGalleryContainer = document.getElementById('image-gallery-container');
+    if (normalView) {
+      setNormalView(false);
+      img.style.cursor = 'cell';
+      img.style.overflow = 'hidden';
+      imageGalleryContainer.style['z-index'] = 5;
+      imageGalleryContainer.style.width = 'auto';
       img.style.transform = 'scale(1.5)';
       img.style.height = 'auto';
       img.style.width = 'auto';
+      img.style['object-fit'] = 'cover';
       img.style.transition = 'transform 0.25s ease';
       setExpandedView(true);
+    } else if (expandedView) {
+      img.style.cursor = 'zoom-out';
+      img.style.transform = 'scale(1)';
+      img.style.transform = 'scale(2.5)';
+      setExpandedView(false);
+      setZoomedView(true);
+    } else {
+      img.style.cursor = 'zoom-in';
+      img.style.transform = 'scale(1)';
+      img.style.height = '500px';
+      img.style.width = '500px';
+      imageGalleryContainer.style['z-index'] = 0;
+      setZoomedView(false);
+      setNormalView(true);
     }
   };
 
   return (
-    <div className="image-gallery-container">
-      <div className="carousel-thumbnail-container">
+    <div id="image-gallery-container">
+      <div id="carousel-thumbnail-container">
         <button className="prev navigate" id="carousel-thumbnail-prev" aria-label="previous" type="button" onClick={getPrev}>&and;</button>
         {thumbnails}
         <button className="next navigate" id="carousel-thumbnail-next" aria-label="next" type="button" onClick={getNext}>&or;</button>
       </div>
-      <div className="carousel-container">
+      <div id="carousel-container">
         <img id="displayed-image" role="presentation" src={photos[index].url} alt={name} onClick={enlargeImage} />
         <div className="carousel-actions">
           <button className="prev navigate" id="carousel-prev" aria-label="previous" type="button" onClick={getPrev}>&lt;</button>
