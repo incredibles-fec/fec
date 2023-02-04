44a3c2d022b014a946207679a8035c0a2c3a3fc9
Patrick Kelly <patrick.will.kelly@gmail.com>
2023-02-04 10:30:19 -0800

    implement sale display functionality

--- client/src/components/itemsComparison/Product.jsx
+++ client/src/components/itemsComparison/Product.jsx
@@ -15,7 +15,13 @@ export default function Product({ item, count }) {
         <i onClick={changeModal} className="fas fa-star" />
         <div className="productCardCategory">{item.category}</div>
         <div className="productCardName">{item.name}</div>
-        <div className="productCardPrice">{item.default_price}</div>
+        {item.salePrice !== null ? (
+          <div className="priceInfo">
+            <div className="productCardSale">{item.salePrice} SALE</div>
+            <div className="productCardPriceSale">{item.price}</div>
+          </div>
+        ) :
+          <div className="productCardPrice">{item.price}</div>}
         {/* <div className="productCardRating">STAR RATING</div> */}
       </div>
       <ProductModal visible={visibleStatus} onClick={changeModal} />
