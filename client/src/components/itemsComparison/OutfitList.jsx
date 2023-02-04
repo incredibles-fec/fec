c6841fa660c6191cfbb00eae2633f6cd64f00355
Patrick Kelly <patrick.will.kelly@gmail.com>
2023-02-03 17:34:42 -0800

    implement axios calls to fetch all relevant product information

--- client/src/components/itemsComparison/OutfitList.jsx
+++ client/src/components/itemsComparison/OutfitList.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import axios from 'axios';
 import Outfit from './Outfit.jsx';
 
 export default function OutfitList() {
--- client/src/components/itemsComparison/ProductList.jsx
+++ client/src/components/itemsComparison/ProductList.jsx
@@ -1,54 +1,82 @@
 import React from 'react';
 import axios from 'axios';
+import { useSelector } from 'react-redux';
 import Product from './Product.jsx';
 
 export default function ProductList() {
+  const [relatedList, setRelatedList] = React.useState([]);
+  const { currentProduct, currentProductStyles } = useSelector((state) => state.pd);
   const [products, setProducts] = React.useState([]);
-  const [styles, setStyles] = React.useState([]);
   const [previousVisble, setPreviousVisble] = React.useState(false);
   const [nextVisible, setnextVisible] = React.useState(true);
   const [firstSlide, setFirstSlide] = React.useState(0);
-  const [lastSlide, setLastSlide] = React.useState(4);
+  const [lastSlide, setLastSlide] = React.useState(3);
 
   const obtainProducts = () => {
-    axios.get('/products')
+    const productId = currentProduct.id;
+    axios.get(`/products/${productId}/related`)
       .then((response) => {
-        setProducts(response.data);
-        const productId = response.data[0].id;
-        axios.get(`/products/${productId}/styles`)
-          .then((styleResponse) => {
-            console.log(styleResponse);
-            setStyles(styleResponse);
-          });
+        const options = [];
+        const relatedProducts = response.data;
+        relatedProducts.forEach((related) => {
+          axios.get(`/products/${related}`)
+            .then((idResponse) => {
+              options.push(idResponse.data);
+              setRelatedList([...relatedList, idResponse.data]);
+              setRelatedList(options);
+
+              axios.get(`/products/${related}/styles`)
+                .then((styleInfo) => {
+                  const photo = styleInfo.data.results[0].photos[0].thumbnail_url;
+                  const price = styleInfo.data.results[0].original_price;
+                  const salePrice = styleInfo.data.results[0].sale_price;
+                  const id = styleInfo.data.product_id;
+                  options.forEach((product) => {
+                    console.log('productID ', product.id);
+                    console.log('id ', id);
+                    if (product.id.toString() === id) {
+                      console.log('TRUE');
+                      product.image = photo;
+                      product.price = price;
+                      product.salePrice = salePrice;
+                    }
+                  });
+
+                  setRelatedList([...relatedList, styleInfo.data]);
+                  setRelatedList(options);
+                  console.log('OPTIONS ', options);
+                });
+            })
+            .catch((err) => {
+              console.log('product ID fetch failed with err ', err);
+            });
+        });
       })
       .catch((error) => {
         console.log('fetch of products failed with error: ', error);
       });
   };
 
-  // obtain products order of operations
-  // user selects a specific product
-    // make call to related products endpoint (products/productId/related)
-      // receive list of related products (array of ids)
-      // create variable to store newlist (will become an array of objects)
-      // iterate over list of related products - for each product
-        // make call to product id endpoint (products/productId)
-          // (response will be an object)
-          // add response to new list of products
-          // capture product id of current product (response.id)
-          // make a call to products/product_id/styles
-            // add responses image, price/sale price to relevant object in newlist
-            //// response[0].original_price, response[0].sale_price, response[0].photos[0].thumbnail_url
-      // set currentproducts equal to newlist
+  // add responses image, price/sale price to relevant object in newlist
+  // set currentproducts equal to newlist
+
+  React.useEffect(() => {
+    if (currentProduct !== null) {
+      obtainProducts();
+    }
+  }, currentProduct);
+
+  // React.useEffect(() => {
+  //   obtainProducts();
+  // }, []);
 
   const onNext = () => {
     if (firstSlide >= 0) {
       setPreviousVisble(true);
     }
-    if (lastSlide >= 9) {
+    if (lastSlide >= relatedList.length - 1) {
       setnextVisible(false);
     }
-
     const cardToView = document.getElementById(lastSlide);
     cardToView.scrollIntoView();
     setFirstSlide(firstSlide + 1);
@@ -58,7 +86,7 @@ export default function ProductList() {
   const onBack = () => {
     if (firstSlide === 0) {
       setPreviousVisble(false);
-    } else if (lastSlide >= 9) {
+    } else if (lastSlide >= relatedList.length - 1) {
       setnextVisible(true);
     }
     const cardToView = document.getElementById(firstSlide);
@@ -67,10 +95,6 @@ export default function ProductList() {
     setLastSlide(lastSlide - 1);
   };
 
-  React.useEffect(() => {
-    obtainProducts();
-  }, []);
-
   let num = 0;
 
   return (
@@ -78,14 +102,13 @@ export default function ProductList() {
       <h3>Related Products</h3>
       <div className="relatedProductsCarousel">
         <div className="relatedProductContainer">
-          {products.map((item) => (<Product item={item} key={item.id} count={num++}/>))}
+          {relatedList.map((item) => (<Product item={item} key={item.id} count={num++} />))}
         </div>
         <div className="carouselActions">
           { previousVisble ?
-          <button type="button" className="previousProduct" onClick={onBack}>&lt;</button> : null }
+            <button type="button" className="previousProduct" onClick={onBack}>&lt;</button> : null }
           { nextVisible ?
-            <button type="button" className="nextProduct" onClick={onNext}>&gt;</button> : null
-          }
+            <button type="button" className="nextProduct" onClick={onNext}>&gt;</button> : null}
         </div>
       </div>
     </div>
