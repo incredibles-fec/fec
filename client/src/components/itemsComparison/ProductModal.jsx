9997bc9dd61c96cae4c5db00688a7a3ea2ffb4f6
Patrick Kelly <patrick.will.kelly@gmail.com>
2023-02-11 12:33:36 -0800

    implement blur of background upon open of product comparison modal

--- client/src/components/itemsComparison/ProductModal.jsx
+++ client/src/components/itemsComparison/ProductModal.jsx
@@ -50,50 +50,44 @@ export default function ProductModal({
 
   return (
     <div>
-      <div className="productModal">
-        <h1>PRODUCT COMPARISON</h1>
-        <i className="fa-solid fa-square-xmark" onClick={(e) => onClick(e)} />
-        <table>
-          {/* <thead>
-            <tr className="modalHeader">
-              <th className="leftHeader">Current Viewed Product</th>
-              <th className="rightHeader">Compared Product Card</th>
-            </tr>
-          </thead> */}
-        </table>
-        <table className="compareTable">
-          <thead>
-            <tr>
-              <th className="checkLeftHeader" aria-label="left check header" />
-              <th className="productInfo" aria-label="product information" />
-              <th className="checkRightHeader" aria-label="right check header" />
-            </tr>
-            { Object.values(finalObject).length > 0 ?
-              Object.keys(finalObject).map((product) => {
-                if (product === 'name') {
-                  return (
-                    <tr key={product}>
-                      <td className="productNameLeft">{finalObject[product][0] || '--'}</td>
-                      <td />
-                      <td className="productNameRight">{finalObject[product][1] || '--'}</td>
-                    </tr>
-                  );
-                } else if (product !== 'features' && product !== 'image' && product !== 'slogan' && product !== 'description' && product !== 'price' && product !== 'id' && product !== 'salePrice' && product !=='name') {
-                  return (
-                    <tr key={product}>
-                      <td>{finalObject[product][0] || '--'}</td>
-                      {product === 'default_price' ?
-                        <td className="productInfo">price</td> :
-                        <td className="productInfo">{product.toLowerCase()}</td>}
-                      <td>{finalObject[product][1] || '--'}</td>
-                    </tr>
-                  );
-                }
-              })
-              : null}
-          </thead>
-        </table>
-      </div>
+      <div className="modal">
+        <div className="productModal">
+          <h1>PRODUCT COMPARISON</h1>
+          <i className="fa-solid fa-square-xmark fa-xl" onClick={(e) => onClick(e)} />
+          <table className="compareTable">
+            <thead>
+              <tr>
+                <th className="checkLeftHeader" aria-label="left check header" />
+                <th className="productInfo" aria-label="product information" />
+                <th className="checkRightHeader" aria-label="right check header" />
+              </tr>
+              { Object.values(finalObject).length > 0 ?
+                Object.keys(finalObject).map((product) => {
+                  if (product === 'name') {
+                    return (
+                      <tr key={product}>
+                        <td className="productNameLeft">{finalObject[product][0] || '--'}</td>
+                        <td />
+                        <td className="productNameRight">{finalObject[product][1] || '--'}</td>
+                      </tr>
+                    );
+                  } else if (product !== 'features' && product !== 'image' && product !== 'slogan' && product !== 'description' && product !== 'price' && product !== 'id' && product !== 'salePrice' && product !=='name') {
+                    return (
+                      <tr key={product}>
+                        <td>{finalObject[product][0] || '--'}</td>
+                        {product === 'default_price' ?
+                          <td className="productInfo">price</td> :
+                          <td className="productInfo">{product.toLowerCase()}</td>}
+                        <td>{finalObject[product][1] || '--'}</td>
+                      </tr>
+                    );
+                  }
+                })
+                : null}
+            </thead>
+          </table>
+        </div>
+    </div>
     </div>
   );
 }
